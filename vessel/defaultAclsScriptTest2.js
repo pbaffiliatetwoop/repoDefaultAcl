054781ea82118a2c2739483d9d11711438ff77bc
pbaffiliatetwoop <10269519+pbaffiliatetwoop@users.noreply.github.com>
2022-01-28 08:30:54 +0200

    Commit from Test -  vessel/defaultAclsScriptTest2.js

---
+++ vessel/defaultAclsScriptTest2.js
@@ -0,0 +1,13 @@
+<script>
+<scriptACL>
+<execute>authenticated</execute>
+<read>authenticated</read>
+<write>authenticated</write>
+</scriptACL>
+<code> 
+<![CDATA[
+
+         systemLogger.setLevel("off");
+         return "heavy metal will not die";]]>
+</code>
+</script>
